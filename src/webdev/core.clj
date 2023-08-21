c386e5dbf0311a1cf53b375c47cc716ffc5b588f
kraulain <kraulain@gmail.com>
2023-08-21 06:31:47 +0100

    Add and configure main function as entry point

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,7 +1,9 @@
 (ns webdev.core
-  (require [ring.adapter.jetty :as jetty]))
+  (:require [ring.adapter.jetty :as jetty]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main [port]
+  (jetty/run-jetty (fn [req]
+                     {:status 200
+                      :body "Hello, World!"
+                      :headers {}})
+                   {:port (Integer. port)}))
