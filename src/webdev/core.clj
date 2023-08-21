de8d71cce916ac646b4816e239a6cef79ed40679
kraulain <kraulain@gmail.com>
2023-08-21 06:52:59 +0100

    Add basic routing

--- src/webdev/core.clj
+++ src/webdev/core.clj
@@ -1,9 +1,12 @@
 (ns webdev.core
   (:require [ring.adapter.jetty :as jetty]))
 
+(defn greet [req]
+  (case (:uri req)
+    "/"     {:Status 200 :body "Hello, World!" :headers {}}
+    "/ping" {:Status 200 :body "Ping ping" :headers {}}
+            {:Status 404 :body "Not Found" :headers {}}))
+
 (defn -main [port]
-  (jetty/run-jetty (fn [req]
-                     {:status 200
-                      :body "Hello, World!"
-                      :headers {}})
+  (jetty/run-jetty greet
                    {:port (Integer. port)}))
